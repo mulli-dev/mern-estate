8eb9c494e0f40d9625a3af01578122013b4aa206
Jacob Mulli <jacobmullie2@gmail.com>
2024-02-10 00:13:11 +0300

    create get user listings API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -12,6 +12,7 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+
 router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
