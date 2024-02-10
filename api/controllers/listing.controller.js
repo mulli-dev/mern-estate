2c006eb48826d8d957bea565da3bc548b6575d5d
Jacob Mulli <jacobmullie2@gmail.com>
2024-02-10 14:35:10 +0300

    create update listing api route backend

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -28,3 +28,24 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
