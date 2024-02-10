9f18c591ab6f8ea7affc0ce01a055eca47e3bb56
Jacob Mulli <jacobmullie2@gmail.com>
2024-02-10 21:27:17 +0300

    complete update listing functionality edit link

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,12 +3,15 @@ import {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 
 export default router;
