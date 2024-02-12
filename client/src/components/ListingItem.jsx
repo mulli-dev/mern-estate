5ec152b6d5bc59f99370cdf0f4d5919c60915571
Jacob Mulli <jacobmullie2@gmail.com>
2024-02-13 00:45:32 +0300

    Create listing item component s and show listings

---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
+
+export default function ListingItem({ listing }) {
+  return (
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={
+            listing.imageUrls[0] ||
+            "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
+          }
+          alt="listing cover"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+        />
+        <div className="flex flex-col w-full gap-2 p-3">
+          <p className="text-lg font-semibold truncate text-slate-700">
+            {listing.name}
+          </p>
+          <div className="flex items-center gap-1">
+            <MdLocationOn className="w-4 h-4 text-green-700" />
+            <p className="w-full text-sm text-gray-600 truncate">
+              {listing.address}
+            </p>
+          </div>
+          <p className="text-sm text-gray-600 line-clamp-2">
+            {listing.description}
+          </p>
+          <p className="mt-2 font-semibold text-slate-500 ">
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && "/ month"}
+          </p>
+          <div className="flex gap-4 text-slate-700 ">
+            <div className="text-xs font-bold">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds `
+                : `${listing.bedrooms} bed `}
+            </div>
+            <div className="text-xs font-bold">
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} baths `
+                : `${listing.bathrooms} bath `}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
+
 export default function Search() {
   const navigate = useNavigate();
 
@@ -219,6 +221,22 @@ export default function Search() {
         <h1 className="p-3 mt-5 text-3xl font-semibold border-b text-slate-700">
           Listing results:
         </h1>
+        <div className="flex flex-wrap gap-4 p-7 ">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found!</p>
+          )}
+          {loading && (
+            <p className="w-full text-xl text-center text-slate-700">
+              Loading...
+            </p>
+          )}
+
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
       </div>
     </div>
   );
